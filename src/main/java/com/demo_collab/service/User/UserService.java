86ecb53e23c7a25c992f2b0dbcda4acb917400ce
luc-nguyen-174 <louiskingvn@gmail.com>
2023-03-13 01:25:41 +0700

    updating commit and rollback

--- src/main/java/com/demo_collab/service/User/UserService.java
+++ src/main/java/com/demo_collab/service/User/UserService.java
@@ -1,5 +1,6 @@
 package com.demo_collab.service.User;
 
+import com.demo_collab.connect.Connect;
 import com.demo_collab.model.User;
 
 import java.sql.Connection;
@@ -12,8 +13,15 @@ import java.util.List;
 import static com.demo_collab.connect.Connect.getConnection;
 
 public class UserService implements IUser {
-    public UserService() {
-    }
+//    private final Connection connection;
+//
+//    {
+//        try {
+//            connection = Connect.getConnection();
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
     private static final String SELECT_ALL_USERS = "select * from user";
     private static final String INSERT = "insert into user (user_id, user_name, phone, province_id, role_id) values (?,?,?,?,?)";
@@ -22,65 +30,105 @@ public class UserService implements IUser {
     @Override
     public List<User> findAll() {
         List<User> users = new ArrayList<>();
-        try (Connection connection = getConnection();
-             PreparedStatement statement = connection.prepareStatement(SELECT_ALL_USERS);
-        ) {
-            System.out.println(statement);
-            ResultSet rs = statement.executeQuery();
+        if (connection != null) {
+            try (
+                    PreparedStatement statement = connection.prepareStatement(SELECT_ALL_USERS)
+            ) {
+                System.out.println(statement);
+                ResultSet rs = statement.executeQuery();
 
-            while (rs.next()) {
-                int id = rs.getInt("user_id");
-                String name = rs.getString("user_name");
-                String phone = rs.getString("phone");
-                int province_id = rs.getInt("province_id");
-                int role_id = rs.getInt("role_id");
-                users.add(new User(id, name, phone, province_id, role_id));
+                while (rs.next()) {
+                    int id = rs.getInt("user_id");
+                    String name = rs.getString("user_name");
+                    String phone = rs.getString("phone");
+                    int province_id = rs.getInt("province_id");
+                    int role_id = rs.getInt("role_id");
+                    users.add(new User(id, name, phone, province_id, role_id));
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            return users;
         }
-        return users;
     }
 
     @Override
     public void save(User user) {
-        try (Connection connection = getConnection();
-             PreparedStatement statement = connection.prepareStatement(INSERT)
-        ) {
+//        if (connection != null) {
+//            try (
+//                    PreparedStatement statement = connection.prepareStatement(INSERT)
+//            ) {
+//                connection.setAutoCommit(false);
+//                statement.setInt(1, user.getUser_id());
+//                statement.setString(2, user.getUser_name());
+//                statement.setString(3, user.getPhone());
+//                statement.setInt(4, user.getProvince_id());
+//                statement.setInt(5, user.getRole_id());
+//                connection.commit();
+//                connection.setAutoCommit(true);
+//                statement.executeQuery();
+//            } catch (SQLException e) {
+//                try {
+//                    connection.rollback();
+//                } catch (SQLException ex) {
+//                    throw new RuntimeException(ex);
+//                }
+//            } finally {
+//                try {
+//                    connection.close();
+//                } catch (SQLException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+//        }
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        try {
+            connection = getConnection();
             connection.setAutoCommit(false);
-            statement.setInt(1, user.getUser_id());
-            statement.setString(2, user.getUser_name());
-            statement.setString(3, user.getPhone());
-            statement.setInt(4, user.getProvince_id());
-            statement.setInt(5, user.getRole_id());
-            connection.commit();
-            connection.setAutoCommit(true);
-            statement.executeQuery();
+            statement = connection.prepareStatement(INSERT);
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 
     @Override
     public User findById(int id) {
         User users = null;
-        try (Connection conn = getConnection();
-        PreparedStatement statement = conn.prepareStatement(SELECT_USER_BY_ID);
-        ) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        try {
+            connection = getConnection();
+            connection.setAutoCommit(false);
+            statement = connection.prepareStatement(SELECT_USER_BY_ID);
             statement.setInt(1, id);
-            ResultSet rs = statement.executeQuery();
+            rs = statement.executeQuery();
             while (rs.next()) {
-                conn.setAutoCommit(false);
                 String userName = rs.getString("user_name");
                 String phone = rs.getString("phone");
                 int province_id = rs.getInt("province_id");
                 int role_id = rs.getInt("role_id");
                 users = new User(id, userName, phone, province_id, role_id);
-                conn.commit();
-                conn.setAutoCommit(true);
             }
+            connection.commit();
+            connection.setAutoCommit(true);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (statement != null) statement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
         return users;
     }
