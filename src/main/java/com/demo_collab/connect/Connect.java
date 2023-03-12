1c8b7076a2237bdc916dc1d1912bc9766d27ae3e
luc-nguyen-174 <louiskingvn@gmail.com>
2023-03-12 20:47:53 +0700

    share connect

---
+++ src/main/java/com/demo_collab/connect/Connect.java
@@ -0,0 +1,22 @@
+package com.demo_collab.connect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+    private static final String jdbcURL = "jdbc:mysql://localhost:3306/demo_user_management?useSSL=false";
+    private static final String jdbcUsername = "root";
+    private static final String jdbcPassword = "Nguyenluc97";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
